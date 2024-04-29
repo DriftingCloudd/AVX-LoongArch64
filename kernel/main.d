4091ebcea86cde94679a17dbe114457ec7ec77b9
DriftingCloud <driftingcloudd@outlook.com>
2024-04-29 17:20:25 +0800

    完善了loongarchregs.h与loongarch.h文件，完成了entry.S到main.c的启动，初步修改了ld与makefile文件。已经是一个能编译的项目。理论上能在模拟器上跑了。以后的修改可以在此基础上进行

--- kernel/main.d
+++
@@ -1,3 +0,0 @@
-kernel/main.o: kernel/main.c kernel/include/loongarch.h \
- /opt/loongson-gnu-toolchain-8.3-x86_64-loongarch64-linux-gnu-rc1.2/lib/gcc/loongarch64-linux-gnu/8.3.0/include/larchintrin.h \
- kernel/include/types.h kernel/include/loongarchregs.h

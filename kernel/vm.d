508453a8a0a4cd86820ec6644b7533d2c9b7f38a
wfftty <2508570302@qq.com>
2024-05-11 17:06:29 +0800

    wait for trapframe

--- kernel/vm.d
+++
@@ -1,8 +0,0 @@
-kernel/vm.o: kernel/vm.c kernel/include/vm.h kernel/include/types.h \
- kernel/include/loongarch.h \
- /opt/loongson-gnu-toolchain-8.3-x86_64-loongarch64-linux-gnu-rc1.2/lib/gcc/loongarch64-linux-gnu/8.3.0/include/larchintrin.h \
- kernel/include/proc.h kernel/include/param.h kernel/include/spinlock.h \
- kernel/include/context.h kernel/include/kalloc.h \
- kernel/include/memlayout.h kernel/include/param.h \
- kernel/include/printf.h kernel/include/proc.h kernel/include/loongarch.h \
- kernel/include/string.h kernel/include/types.h
